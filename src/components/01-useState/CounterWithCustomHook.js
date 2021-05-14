1c767aabcc6c253a7184f0f58cff58f0543fd70a
Martin Obermeier <martin.obermeier@gmail.com>
2021-05-14 20:42:03 -0300

    useState y useEffect

---
+++ src/components/01-useState/CounterWithCustomHook.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useCounter } from '../../hooks/useCounter';
+
+import './counter.css';
+
+export const CounterWithCustomHook = () => {
+
+    const { state, increment, decrement, reset } = useCounter( 100 );
+
+    return (
+        <>
+            <h1>Counter with Hook: { state }</h1>
+            <hr />
+
+            <button className="btn btn-primary" onClick={ () => increment(2) } > + 1 </button>
+            <button className="btn btn-info" onClick={ reset } > reset </button>
+            <button className="btn btn-danger" onClick={ () => decrement(2) } > - 1 </button>
+        </>
+    )
+}
